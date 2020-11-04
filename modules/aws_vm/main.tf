00c89b30b89ee82d6c80a4967f7af2c816abf72e
Matthias Ender <matthias.ender@sas.com>
2020-11-04 04:30:33 -0500

    simplify ssh key generation     use the same key for all VMs, output ssh_public_key, ssh_private_key only, if keys are generated.     Fixes https://rndjira.sas.com/browse/IAC-65

--- modules/aws_vm/main.tf
+++ modules/aws_vm/main.tf
@@ -2,7 +2,7 @@
 
 # Hack for assigning disk in a vm based on an index value. 
 locals {
-  device_name = ["b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  device_name = ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 }
 
 data "aws_ami" "centos" {
@@ -29,24 +29,13 @@ data "null_data_source" "ebs_disk" {
   count = var.data_disk_count
 }
 
-resource "tls_private_key" "private_key" {
-  count = var.ssh_public_key == "" ? 1 : 0
-  algorithm = "RSA"
-}
-
-data "tls_public_key" "public_key" {
-  count = var.ssh_public_key == "" ? 1 : 0
-  private_key_pem = element(coalescelist(tls_private_key.private_key.*.private_key_pem), 0)
-}
-
 locals {
-  ssh_public_key = var.ssh_public_key != "" ? file(var.ssh_public_key) : element(coalescelist(data.tls_public_key.public_key.*.public_key_openssh, [""]), 0)
-  vm_admin       = var.vm_admin
+  vm_admin = var.vm_admin
 }
 
 resource "aws_key_pair" "admin" {
-  key_name = "${var.name}-admin"
-  public_key = local.ssh_public_key
+  key_name   = "${var.name}-admin"
+  public_key = var.ssh_public_key
 }
 
 resource "aws_instance" "vm" {
@@ -57,13 +46,13 @@ resource "aws_instance" "vm" {
   key_name      = aws_key_pair.admin.key_name
 
   vpc_security_group_ids = var.security_group_ids
-  subnet_id = var.subnet_id
+  subnet_id              = var.subnet_id
 
   root_block_device {
-    volume_type = var.os_disk_type
-    volume_size = var.os_disk_size
+    volume_type           = var.os_disk_type
+    volume_size           = var.os_disk_size
     delete_on_termination = var.os_disk_delete_on_termination
-    iops = var.os_disk_iops
+    iops                  = var.os_disk_iops
   }
 
   tags = merge(var.tags, map("Name", "${var.name}-vm"))
