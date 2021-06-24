61b7da6911c416d0d6298e969641a11f1f16b3e7
Thomas S. Pangborn <thomas.pangborn@sas.com>
2021-06-24 08:07:26 -0400

    Restructure code

--- modules/aws_autoscaling/main.tf
+++ modules/aws_autoscaling/main.tf
@@ -1,10 +1,3 @@
-resource "aws_iam_policy" "worker_autoscaling" {
-  name_prefix = "${var.prefix}-eks-worker-autoscaling"
-  description = "EKS worker node autoscaling policy for cluster ${var.cluster_name}"
-  policy      = data.aws_iam_policy_document.worker_autoscaling.json
-  tags        = var.tags
-}
-
 data "aws_iam_policy_document" "worker_autoscaling" {
   statement {
     sid    = "eksWorkerAutoscalingAll"
@@ -47,23 +40,26 @@ data "aws_iam_policy_document" "worker_autoscaling" {
   }
 }
 
+resource "aws_iam_policy" "worker_autoscaling" {
+  name_prefix = "${var.prefix}-eks-worker-autoscaling"
+  description = "EKS worker node autoscaling policy for cluster ${var.cluster_name}"
+  policy      = data.aws_iam_policy_document.worker_autoscaling.json
+  tags        = var.tags
+}
+
 module "iam_assumable_role_with_oidc" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "4.1.0"
 
   create_role = true
-
   role_name = "${var.prefix}-cluster-autoscaler"
+  provider_url = replace(var.oidc_url, "https://", "")
+  role_policy_arns = [aws_iam_policy.worker_autoscaling.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:cluster-autoscaler"]
 
   tags = {
     Role = "${var.prefix}-cluster-autoscaler"
   }
-
-  provider_url = replace(var.oidc_url, "https://", "")
-
-  role_policy_arns = [aws_iam_policy.worker_autoscaling.arn]
-
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:cluster-autoscaler"]
 
 }
 
