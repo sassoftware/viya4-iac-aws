2d64258c451a4e7a767b7ea9f5ac894c04bb5678
Mano Meenaksh <mano.meenaksh@sas.com>
2022-03-30 11:30:43 -0400

    revert all versions except aws provider required by EKS module

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 provider "aws" {
   region                   = var.location
   profile                  = var.aws_profile
-  shared_credentials_files = [var.aws_shared_credentials_file]
+  shared_credentials_file  = var.aws_shared_credentials_file
   access_key               = var.aws_access_key_id
   secret_key               = var.aws_secret_access_key
   token                    = var.aws_session_token
