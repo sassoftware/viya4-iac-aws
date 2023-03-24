1e125c2f3e0cf9df2f9bbb949e0c72ebec030b8c
Anthony D. Davis <116221986+anthoday@users.noreply.github.com>
2023-03-24 09:46:02 -0500

    doc: (IAC-935) Added copyright for iac-aws github projects (#196)
    * added copyright information - initial
    
    * added copyright to required .sh files
    
    * added copyright to required .tf files
    
    * adding copyright

--- files/custom-data/additional_userdata.sh
+++ files/custom-data/additional_userdata.sh
@@ -1,3 +1,6 @@
+# Copyright © 2021-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 ##
 # 
 # For AWS this is a script 'snippet' that is injected into another script
--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+# Copyright © 2021-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 ## Global
 variable "prefix" {
   description = "A prefix used in the name for all cloud resources created by this script. The prefix string must start with a lowercase letter and contain only alphanumeric characters and hyphens or dashes (-), but cannot start or end with '-'."
