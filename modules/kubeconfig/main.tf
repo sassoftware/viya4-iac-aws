ca62250cb40dcbdfa6e935e4ba99626073971642
David.Houck <David.Houck@sas.com>
2023-05-22 21:35:04 +0000

    updates for tflint warnings

--- modules/kubeconfig/main.tf
+++ modules/kubeconfig/main.tf
@@ -1,6 +1,20 @@
 # Copyright © 2021-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+terraform {
+  required_version = ">= 1.4.5"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
+  }
+}
+
 locals {
   service_account_name        = "${var.prefix}-cluster-admin-sa"
   cluster_role_binding_name   = "${var.prefix}-cluster-admin-crb"
