eee7546bf4ce59c406141e6ab7b1e1b1827a5d3a
jarpat <jay.patel@sas.com>
2023-09-28 10:26:42 -0400

    rename output.tf to outputs.tf

--- modules/kubeconfig/outputs.tf
+++ modules/kubeconfig/outputs.tf
@@ -1,7 +1,6 @@
 # Copyright © 2021-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# tflint-ignore: terraform_standard_module_structure
 output "kube_config" {
   value = local_file.kubeconfig.content
 }
