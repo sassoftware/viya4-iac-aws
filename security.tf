e952b307311fdc9286499882ac68ab9255055d18
David.Houck <David.Houck@sas.com>
2023-10-20 17:27:33 +0000

    Update ref to local var, allow existing workers SG input without cluster SG input

--- security.tf
+++ security.tf
@@ -116,7 +116,7 @@ resource "aws_security_group_rule" "cluster_ingress" {
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
-  source_security_group_id = aws_security_group.workers_security_group[0].id
+  source_security_group_id = local.workers_security_group_id
   security_group_id        = local.cluster_security_group_id
 }
 
