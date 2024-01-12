79bd167218e48e78c579559928e611a1fed1b441
David Houck <dhoucgitter@users.noreply.github.com>
2024-01-12 11:53:40 -0500

    feat: (IAC-1259) AWS - Security scan 2024.01 (#261)

--- modules/aws_autoscaling/main.tf
+++ modules/aws_autoscaling/main.tf
@@ -1,15 +1,6 @@
 # Copyright © 2021-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-terraform {
-  required_version = ">= 1.4.5"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.25.0"
-    }
-  }
-}
 
 # Permissions based off the IAM Policy recommended by kubernetes/autoscaler
 # https://github.com/kubernetes/autoscaler/blob/cluster-autoscaler-chart-9.25.0/cluster-autoscaler/cloudprovider/aws/README.md
@@ -69,7 +60,7 @@ resource "aws_iam_policy" "worker_autoscaling" {
 
 module "iam_assumable_role_with_oidc" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.30.2"
+  version = "~> 5.0"
 
   create_role                   = true
   role_name                     = "${var.prefix}-cluster-autoscaler"
