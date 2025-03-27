1530f506fd92efb0fc8c6e3d28fc90354f47be22
Chris Miller <53184971+saschjmil@users.noreply.github.com>
2025-03-27 10:16:42 -0400

    Update files/custom-data/additional_userdata.sh

--- files/custom-data/additional_userdata.sh
+++ files/custom-data/additional_userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2021-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# Copyright 2021-2025, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 ##
