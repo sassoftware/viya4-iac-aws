e894c9c3c8b295786489c04d8106a8940c19631e
Thomas S. Pangborn <32514746+thpang@users.noreply.github.com>
2025-06-10 08:08:43 -0400

    Updated copyright

--- modules/aws_ebs_csi/main.tf
+++ modules/aws_ebs_csi/main.tf
@@ -1,4 +1,4 @@
-# Copyright © 2021-2024, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# Copyright © 2021-2025, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 # IAM policy for the EBS CSI driver in an EKS cluster
