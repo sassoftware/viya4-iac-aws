ac6e972c60eaf9c2919f086172a4c4d871151d63
Thomas S. Pangborn <32514746+thpang@users.noreply.github.com>
2025-06-10 08:30:37 -0400

    Updated copyright

--- security.tf
+++ security.tf
@@ -1,4 +1,4 @@
-# Copyright © 2021-2024, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# Copyright © 2021-2025, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 # Data source to fetch the existing security group details if security_group_id is provided
