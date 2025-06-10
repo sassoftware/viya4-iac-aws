76d5bf11fae465c45df3f07874de2902f6d7d789
Thomas S. Pangborn <32514746+thpang@users.noreply.github.com>
2025-06-10 08:29:47 -0400

    Updated copyright

--- modules/kubeconfig/outputs.tf
+++ modules/kubeconfig/outputs.tf
@@ -1,4 +1,4 @@
-# Copyright © 2021-2024, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# Copyright © 2021-2025, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 # The `kube_config` output provides the necessary credentials and endpoint information
